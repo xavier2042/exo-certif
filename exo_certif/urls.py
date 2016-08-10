1f4f68dce93743a0849df3a82d74d0f04a51513a
xavier2042 <xavier2042@gmail.com>
2016-08-10 16:08:45 +0200

    Adding creation item, Updated item and start templates

--- exo_certif/urls.py
+++ exo_certif/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.conf.urls import url, include
 from django.contrib import admin
 
@@ -22,3 +25,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include("posts.urls", namespace="posts")),
 ]
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
+from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import PostForm
 from .models import Post
@@ -10,10 +11,12 @@ def post_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        messages.success(request, "Successfully Created")
+        return HttpResponseRedirect(instance.get_absolute_url())
     context = {
         "form":form,
     }
-    return HttpResponseRedirect(instance.get_absolute_url())
+    return render(request, "post_form.html", context)
 
 def post_detail(request, id=None):
     instance = get_object_or_404(Post, id=id)
@@ -29,7 +32,7 @@ def post_list(request):
         "object_list": queryset,
         "title": "list"
     }
-    return render(request, "index.html", context)
+    return render(request, "post_list.html", context)
 
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
@@ -38,8 +41,8 @@ def post_update(request, id=None):
         instance = form.save(commit=False)
         instance.save()
         # message success
+        messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html-safe')
         return HttpResponseRedirect(instance.get_absolute_url())
-
     context = {
         "title": instance.title,
         "instance": instance,
@@ -47,5 +50,8 @@ def post_update(request, id=None):
     }
     return render(request, "post_form.html", context)
 
-def post_delete(request):
-    return HttpResponse("<h1>Delete</h1>")
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Successfully Deleted")
+    return redirect("posts:list")
