f5a3fb8f920bb39de62e325b2faee2cd590139ea
xavier2042 <xavier2042@gmail.com>
2016-08-10 13:22:03 +0200

    first commit

---
+++ posts/views.py
@@ -0,0 +1,51 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+
+from .forms import PostForm
+from .models import Post
+
+
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+    context = {
+        "form":form,
+    }
+    return HttpResponseRedirect(instance.get_absolute_url())
+
+def post_detail(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    context = {
+        "title": instance.title,
+        "instance": instance
+    }
+    return render(request, "post_detail.html", context)
+
+def post_list(request):
+    queryset = Post.objects.all()
+    context = {
+        "object_list": queryset,
+        "title": "list"
+    }
+    return render(request, "index.html", context)
+
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        # message success
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form
+    }
+    return render(request, "post_form.html", context)
+
+def post_delete(request):
+    return HttpResponse("<h1>Delete</h1>")
