ca75a6cb77da3d73630cad064b39340ba9d8bf4c
xavier2042 <xavier2042@gmail.com>
2016-08-11 17:11:36 +0200

    included slug

--- posts/views.py
+++ posts/views.py
@@ -1,72 +1,78 @@
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import PostForm
 from .models import Post
 
-
 def post_create(request):
-    form = PostForm(request.POST or None, request.FILES or None)
-    if form.is_valid():
-        instance = form.save(commit=False)
-        instance.save()
-        messages.success(request, "Successfully Created")
-        return HttpResponseRedirect(instance.get_absolute_url())
-    context = {
-        "form":form,
-    }
-    return render(request, "post_form.html", context)
+	form = PostForm(request.POST or None, request.FILES or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		# message success
+		messages.success(request, "Successfully Created")
+		return HttpResponseRedirect(instance.get_absolute_url())
+	context = {
+		"form": form,
+	}
+	return render(request, "post_form.html", context)
 
-def post_detail(request, id=None):
-    instance = get_object_or_404(Post, id=id)
-    context = {
-        "title": instance.title,
-        "instance": instance
-    }
-    return render(request, "post_detail.html", context)
+def post_detail(request, slug=None):
+	instance = get_object_or_404(Post, slug=slug)
+	context = {
+		"title": instance.title,
+		"instance": instance,
+	}
+	return render(request, "post_detail.html", context)
 
 def post_list(request):
-    queryset_list = Post.objects.all()
-    paginator = Paginator(queryset_list, 5) # Show 5 item per page
-    page_request_var = "Page" # name link page
-    page = request.GET.get(page_request_var)
-    try:
-        queryset = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        queryset = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        queryset = paginator.page(paginator.num_pages)
-    context = {
-        "object_list": queryset,
-        "title": "list",
-        "page_request_var": page_request_var
-    }
-    return render(request, "post_list.html", context)
+	queryset_list = Post.objects.all() #.order_by("-timestamp")
+	paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
+	page_request_var = "page"
+	page = request.GET.get(page_request_var)
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
 
 
-def post_update(request, id=None):
-    instance = get_object_or_404(Post, id=id)
-    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
-    if form.is_valid():
-        instance = form.save(commit=False)
-        instance.save()
-        # message success
-        messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html-safe')
-        return HttpResponseRedirect(instance.get_absolute_url())
-    context = {
-        "title": instance.title,
-        "instance": instance,
-        "form": form
-    }
-    return render(request, "post_form.html", context)
+	context = {
+		"object_list": queryset,
+		"title": "List",
+		"page_request_var": page_request_var
+	}
+	return render(request, "post_list.html", context)
 
-def post_delete(request, id=None):
-    instance = get_object_or_404(Post, id=id)
-    instance.delete()
-    messages.success(request, "Successfully Deleted")
-    return redirect("posts:list")
+
+
+
+
+def post_update(request, slug=None):
+	instance = get_object_or_404(Post, slug=slug)
+	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
+		return HttpResponseRedirect(instance.get_absolute_url())
+
+	context = {
+		"title": instance.title,
+		"instance": instance,
+		"form":form,
+	}
+	return render(request, "post_form.html", context)
+
+
+
+def post_delete(request, slug=None):
+	instance = get_object_or_404(Post, slug=slug)
+	instance.delete()
+	messages.success(request, "Successfully deleted")
+	return redirect("posts:list")
